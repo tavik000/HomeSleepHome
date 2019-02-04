7608a6205a2e96b2ee6c64a2d5db2e9d4144bfc1
unknown <tavik002@gmail.com>
2019-02-04 19:41:59 +0800

    release v1.0.4

--- Assets/Scripts/DontDestory.cs
+++ Assets/Scripts/DontDestory.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class DontDestory : MonoBehaviour
 {
+
+    void Start()
+    {
+        int width = 1920; // or something else
+        int height = 1080; // or something else
+        bool isFullScreen = true; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
+
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+    }
+
     private void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
